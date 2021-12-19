b11c1616bf7823598a884125616e0c85edbfc7c6
martin0123478 <martin_agui97@outlook.es>
2021-12-19 13:22:36 -0600

    generando rutas

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const express = require('express');
 //crear servidor
 const app = express()
 
-//GET
-app.get('/', (req, res)=>{
-res
-});
+//rutas
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen(4000, () =>{
   console.log(`Servidor corriendo en el puerto ${4000}`)
