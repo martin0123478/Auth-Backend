351a675efee7abdb5f1b722b26ef30ba7bcd25b1
martin0123478 <martin_agui97@outlook.es>
2021-12-19 16:43:22 -0600

    haciendo validaciones de campos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const {response} = require('express')
+const { validationResult } = require('express-validator')
 
 const crearUsuario = (req, res = response) =>{
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(400).json({
+      ok:false,
+      errors:errors.mapped()
+    })
+  }
   const {email,name,password} = req.body
   console.log(email,name,password)
   return res.json({
@@ -9,7 +17,15 @@ const crearUsuario = (req, res = response) =>{
   })
 }
 
-const login = (req, res) =>{
+const login = (req, res = response) =>{
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(400).json({
+      ok:false,
+      errors:errors.mapped()
+    })
+  }
+  console.log(errors)
   const {email,password} = req.body
   console.log(email,password)
   return res.json({
--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,16 @@ const { crearUsuario,login,token} = require('../controllers/auth');
 const router = Router();
 
 //crear nuevo usuario
-router.post('/new', crearUsuario);
+router.post('/new',[
+  check('nombre','el nombre es obligatorio').not().isEmpty(),
+  check('email','el email es obligatorio').isEmail(),
+  check('password','el password es obligatorio').isLength({min:6})
+] ,crearUsuario);
 
 //login de usuario
 router.post('/auth',[
-  check()
+  check('email','El email es obligatorio').isEmail(),
+  check('password','El password es obligatorio').isLength({min:6})
 ], login);
 
 //validar token 
