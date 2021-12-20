aa31de8140ab88901965b5112ccc0b25217960d4
martin0123478 <martin_agui97@outlook.es>
2021-12-19 20:00:25 -0600

    creando middleware para crear validación personalizada

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,9 @@
 const {response} = require('express')
-const { validationResult } = require('express-validator')
+
 
 const crearUsuario = (req, res = response) =>{
-  const errors = validationResult(req)
-  if(!errors.isEmpty()){
-    return res.status(400).json({
-      ok:false,
-      errors:errors.mapped()
-    })
-  }
   const {email,name,password} = req.body
-  console.log(email,name,password)
+  
   return res.json({
     ok:true,
     msg:'crear usuario /new'
@@ -18,16 +11,9 @@ const crearUsuario = (req, res = response) =>{
 }
 
 const login = (req, res = response) =>{
-  const errors = validationResult(req)
-  if(!errors.isEmpty()){
-    return res.status(400).json({
-      ok:false,
-      errors:errors.mapped()
-    })
-  }
-  console.log(errors)
+  
   const {email,password} = req.body
-  console.log(email,password)
+  
   return res.json({
     ok:true,
     msg:'Login de usuario'
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
 const { crearUsuario,login,token} = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
@@ -8,13 +9,15 @@ const router = Router();
 router.post('/new',[
   check('nombre','el nombre es obligatorio').not().isEmpty(),
   check('email','el email es obligatorio').isEmail(),
-  check('password','el password es obligatorio').isLength({min:6})
+  check('password','el password es obligatorio').isLength({min:6}),
+  validarCampos
 ] ,crearUsuario);
 
 //login de usuario
 router.post('/auth',[
   check('email','El email es obligatorio').isEmail(),
-  check('password','El password es obligatorio').isLength({min:6})
+  check('password','El password es obligatorio').isLength({min:6}),
+  validarCampos
 ], login);
 
 //validar token 
