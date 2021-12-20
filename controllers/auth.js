7e9e39c974295d2962d5617189eef62b94f5d4de
martin0123478 <martin_agui97@outlook.es>
2021-12-19 23:21:02 -0600

    encriptando la contraseña del usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const {response} = require('express')
 const Usuario = require('../models/Usuario');
+const bcrypt = require('bcryptjs')
 
 const crearUsuario = async(req, res = response) =>{
   const {email,name,password} = req.body
@@ -18,7 +19,8 @@ const crearUsuario = async(req, res = response) =>{
     const dbUser = new Usuario(req.body);
 
     //hashear contraseña
-
+    const salt = bcrypt.genSaltSync();
+    dbUser.password = bcrypt.hashSync(password,salt)
     //general JWT
 
     //Crear usuario de BD
